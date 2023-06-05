3d7a90f910f8665b77a7f916675964ea3e84f080
MAKMOHA <MAKMOHA@MSS-L-MAKMOHA>
2023-06-05 16:07:27 +0800

    Add project files.

---
+++ akmal_test_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("akmal_test_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("akmal_test_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("695E9BD9-BED8-4E7D-81B4-A680107FB75F")]
+[assembly: AssemblyVersion("1.0.0.0")]
